4be68fa277eac65b38413eb7dddce040c0cc7824
David Tran <davidtranhq@gmail.com>
2022-01-01 21:10:23 +0000

    Sync LeetCode submission - Longest Nice Substring (cpp)

--- problems/longest_nice_substring/solution.cpp
+++ problems/longest_nice_substring/solution.cpp
@@ -1,36 +1,29 @@
 /* scratch:
-    hashtable of (letter, index)
-    
-    Y a z a A a y
-   |  |
-        
+
+divide and conquer: O(n)
+
+find not-nice character
+split string into s1 and s2, excluding not-nice character
+solve recursively for s1 and s2
+return bigger of s1 and s2
     
 */
 
 class Solution {
 public:
     string longestNiceSubstring(string s) {
-        unordered_set<char> chs, missing;
         int n = s.size();
-        string longest;
-        auto cmp = [](const string &s1, const string &s2) {
-            return s1.length() < s2.length();
-        };
+        if (n < 2)
+            return "";
+        unordered_set<char> chs(s.begin(), s.end());
         for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                char c = s[j];
-                char opp = islower(c) ? toupper(c) : tolower(c);
-                if (chs.find(opp) == chs.end()) {
-                    missing.insert(opp);
-                }
-                missing.erase(c);
-                if (missing.size() == 0)
-                    longest = max(longest, s.substr(i, j - i + 1), cmp);
-                chs.insert(c);
-            }
-            chs.clear();
-            missing.clear();
+            char c = s[i];
+            if (chs.find(tolower(c)) != chs.end() && chs.find(toupper(c)) != chs.end())
+                continue;
+            string s1 = longestNiceSubstring(s.substr(0, i));
+            string s2 = longestNiceSubstring(s.substr(i + 1, n - i - 1));
+            return s2.size() > s1.size() ? s2 : s1;
         }
-        return longest;
+        return s;
     }
 };
